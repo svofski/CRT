591f5da26c9dc0e1a3ded487dbd976b90fa51510
svo <svofski@gmail.com>
2015-08-03 15:18:05 +0300

    multi pass works on osx
    setting read buffer to FBOs seems to help
    also merged main.py from single pass

--- glsl/main.py
+++ glsl/main.py
@@ -7,6 +7,7 @@ import random
 import pygame.image
 import gl_util, fbo, shader, texture
 import sys
+import time
 
 pygame.display.init()
 pygame.font.init()
@@ -18,6 +19,10 @@ fbos = []
 color_texture = None
 mpass_texture1 = None
 mpass_texture2 = None
+
+# adjustable shader parameters for fine tuning
+filter_gain = 1
+filter_invgain = 1.6
 
 def loadShaders():
     global shaderSources
@@ -38,37 +43,44 @@ def loadSourceAsSurface():
     print len(sys.argv), int(len(sys.argv) == 1)
     return pygame.image.load('testcard.png' if len(sys.argv) == 1 else sys.argv[1])
 
+def updateCaption(fps):
+    pygame.display.set_caption("Composite video simulation %dx%d %3.1ffps" % tuple(list(screen_size) + [fps]))
+
 sourceSurface = loadSourceAsSurface()
 screen_size = sourceSurface.get_size()    
 
-
-icon = pygame.Surface((1,1)); icon.set_alpha(0); pygame.display.set_icon(icon)
-pygame.display.set_caption("Composite video simulation")
+pygame.display.set_icon(sourceSurface)
+updateCaption(0)
 flags = OPENGL|DOUBLEBUF|RESIZABLE
-pygame.display.set_mode(screen_size,flags)
+pygame.display.set_mode(screen_size, flags)
 
 glEnable(GL_BLEND)
-glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA)
+#glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA)
+glBlendFunc(GL_ONE, GL_ZERO)
 
 glEnable(GL_TEXTURE_2D)
 glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE)
 glTexEnvi(GL_POINT_SPRITE,GL_COORD_REPLACE,GL_TRUE)
-
 
 def init():
     global color_texture, mpass_texture1, mpass_texture2
     global fbos
     global shaders, shaderSources
 
+    print 'init(): Screen size=', repr(screen_size)
     shaders = []
 
-    color_texture = texture.Texture2D.from_surf(sourceSurface)
-    mpass_texture1 = texture.Texture2D.from_empty(screen_size)
-    mpass_texture2 = texture.Texture2D.from_empty(screen_size)
+    pygame.display.set_mode(screen_size, flags)
 
-    fbos = [fbo.FBO2D(screen_size), fbo.FBO2D(screen_size)]
+    color_texture = texture.Texture2D.from_surf(sourceSurface)
+    mpass_texture1 = texture.Texture2D.from_empty(sourceSurface.get_size())
+    mpass_texture2 = texture.Texture2D.from_empty(sourceSurface.get_size())
+
+    fbos = [fbo.FBO2D(sourceSurface.get_size()), fbo.FBO2D(sourceSurface.get_size())]
     fbos[0].attach_color_texture(mpass_texture1, 1)
+    fbos[0].set_read(fbos[1], None)
     fbos[1].attach_color_texture(mpass_texture2, 1)
+    fbos[1].set_read(fbos[0], None)
 
     shaders = [shader.Shader([shader.ProgramShaderFragment(source)]) for source in shaderSources]
 
@@ -81,17 +93,17 @@ def deinit():
     del mpass_texture1
     del mpass_texture2
 
-    del fbos[0]
-    del fbos[0]
+    for i in xrange(len(fbos)):
+        del fbos[0]
 
     for i in xrange(len(shaders)):
         del shaders[0]
-    #del shaders[0]
 
 to_add = []
 iterations = -1
 def get_input():
     global iterations, screen_size
+    global filter_gain, filter_invgain
     
     keys_pressed = pygame.key.get_pressed()
     mouse_buttons = pygame.mouse.get_pressed()
@@ -99,7 +111,21 @@ def get_input():
     for event in pygame.event.get():
         if event.type == QUIT: return False
         elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE: return False
+            if event.key == K_ESCAPE: 
+                return False
+            elif event.key == K_q:
+                filter_gain = filter_gain - 0.01
+            elif event.key == K_w:
+                filter_gain = filter_gain + 0.01
+            elif event.key == K_a:
+                filter_invgain = filter_invgain - 0.01
+            elif event.key == K_s:
+                filter_invgain = filter_invgain + 0.01
+            elif event.key == K_z:
+                filter_invgain = filter_invgain - 0.1
+            elif event.key == K_x:
+                filter_invgain = filter_invgain + 0.1
+            print 'Filter gain = %1.3f invgain = %1.3f' % (filter_gain, filter_invgain)
         elif event.type == VIDEORESIZE:
             screen_size = list(event.size)
             #Resizing messes up the OpenGL context.  Make all our objects again.
@@ -109,17 +135,23 @@ def get_input():
 
 def draw_screen_quad():
     glBegin(GL_QUADS)
-    glTexCoord2f(0,0); glVertex2f(             0,             0)
-    glTexCoord2f(1,0); glVertex2f(screen_size[0],             0)
-    glTexCoord2f(1,1); glVertex2f(screen_size[0],screen_size[1])
-    glTexCoord2f(0,1); glVertex2f(             0,screen_size[1])
+    glTexCoord2f(0, 0); glVertex2f(             0,             0)
+    glTexCoord2f(1, 0); glVertex2f(screen_size[0],             0)
+    glTexCoord2f(1, 1); glVertex2f(screen_size[0], screen_size[1])
+    glTexCoord2f(0, 1); glVertex2f(             0, screen_size[1])
     glEnd()
 
+def clear():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
 def draw_update(texture):
+    clear()
     for i in xrange(len(shaders) - 1):
         pingpong = i % 2
 
         fbo.FBO2D.set_write(fbos[pingpong])
+        fbos[pingpong].print_error()
+
         shader.Shader.use(shaders[i])
         shaders[i].pass_vec2("color_texture_sz", screen_size)
         shaders[i].pass_texture_name(texture, texture.texture_id, "color_texture")
@@ -128,11 +160,8 @@ def draw_update(texture):
         else:
             shaders[i].pass_texture_name(mpass_texture2, mpass_texture2.texture_id, "mpass_texture")
 
-        texture.bind()
-        if pingpong == 1:
-            mpass_texture1.bind()
-        else:
-            mpass_texture2.bind()
+        fbos[pingpong].print_error()
+
         draw_screen_quad()
 
     pingpong = (len(shaders) - 1) % 2
@@ -141,38 +170,36 @@ def draw_update(texture):
     shader.Shader.use(shaders[-1])
     shaders[-1].pass_vec2("color_texture_sz", screen_size)
     shaders[-1].pass_texture_name(texture, texture.texture_id, "color_texture")
+
+    shaders[-1].pass_float("filter_gain", filter_gain)
+    shaders[-1].pass_float("filter_invgain", filter_invgain)
+    shaders[-1].pass_texture_name(texture, texture.texture_id, "color_texture")
+
     if pingpong == 1:
         shaders[-1].pass_texture_name(mpass_texture1, mpass_texture1.texture_id, "mpass_texture")
     else:
         shaders[-1].pass_texture_name(mpass_texture2, mpass_texture2.texture_id, "mpass_texture")
 
-    texture.bind()
-    if pingpong == 1:
-        mpass_texture1.bind()
-    else:
-        mpass_texture2.bind()
-
     draw_screen_quad()
 
 def draw():
-    global iterations
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     gl_util.set_view_2D(screen_size)
-
-    draw_update(color_texture)
-    
+    draw_update(color_texture)    
     pygame.display.flip()
 
 def main():
     init()
     
+    iter = 0
     clock = pygame.time.Clock()
     while True:
         if not get_input(): break
         draw()
-        clock.tick(60)
-        #print clock.get_fps()
+        clock.tick(60)        
+        iter += 1
+        if iter > 60:
+            iter -= 60
+            updateCaption(clock.get_fps())
 
     deinit()
     
